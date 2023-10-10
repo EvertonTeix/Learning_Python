d2a629f20a04a15598bf752ca5a42eb3b3a89695
Everton Teixeira <evertonteixe99@alu.ufc.br>
2023-10-10 20:55:21 -0300

    Delete Lista 2 directory

--- Lista 2/questão5.py
+++
@@ -1,8 +0,0 @@
-#Implemente um algoritmo que leia um número e verifique se o mesmo é par
-
-numero = int(input("Digite um número: "))
-
-if(numero % 2 == 0):
-    print("Número par")
-else:
-    print("Numero ímpar")
